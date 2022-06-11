14819156ee869d9a12b8e0ead3548afc010ebc6d
Celestinooo <001celestino@gmail.com>
2022-06-11 14:38:56 -0300

    Criação de controller para tela de venda

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module br.edu.ifsp.makeiteasy.front {
     opens br.edu.ifsp.makeiteasy.front.enums to javafx.fxml;
     opens br.edu.ifsp.makeiteasy.front.utils to javafx.fxml, javafx.base;
     opens br.edu.ifsp.makeiteasy.front.controller to javafx.fxml, javafx.base;
+    opens br.edu.ifsp.makeiteasy.front.controller.venda to javafx.fxml, javafx.base;
     opens br.edu.ifsp.makeiteasy.front.controller.gerenciamento.cliente to javafx.fxml, javafx.base;
     opens br.edu.ifsp.makeiteasy.front.controller.gerenciamento.campanha to javafx.fxml, javafx.base;
     exports br.edu.ifsp.makeiteasy.front;
