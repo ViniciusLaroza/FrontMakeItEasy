fe7ce7057d8813e8bc7ddd084410f2b6d23dc5c8
Celestinooo <001celestino@gmail.com>
2022-06-11 15:33:46 -0300

    Criação de telas de remover

---
+++ src/main/java/br/edu/ifsp/makeiteasy/front/controller/gerenciamento/cliente/RemoverClienteController.java
@@ -0,0 +1,33 @@
+package br.edu.ifsp.makeiteasy.front.controller.gerenciamento.cliente;
+
+import br.edu.ifsp.makeiteasy.front.enums.Tela;
+import br.edu.ifsp.makeiteasy.front.utils.UILoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class RemoverClienteController {
+
+    @FXML
+    private Button buttonCancelar;
+
+    @FXML
+    void initialize(){
+        setButtonsClickListener();
+    }
+
+    private void setButtonsClickListener(){
+        setActionListener(buttonCancelar);
+    }
+    private void setActionListener(Button button){
+        button.setOnAction(actionEvent -> {
+            try {
+                UILoader.substituirTela(Tela.MENU_CLIENTE.getNomeTela());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+}
